1fa0aac5ff0ce1417fc02bcd7143bf20b1c2ac70
AndyTheDragon <andre.jansam@gmail.com>
2024-08-22 09:57:21 +0200

    Ændret til buzz fizz

--- Main/Java/Main.java
+++ Main/Java/Main.java
@@ -4,10 +4,10 @@ public class Main {
         for (int i = 1; i <= 100; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 if (i % 3 == 0) {
-                    System.out.print("Fizz");
+                    System.out.print("Buzz");
                 }
                 if (i % 5 == 0) {
-                    System.out.print("Buzz");
+                    System.out.print("Fizz");
                 }
                 System.out.println();
             }
